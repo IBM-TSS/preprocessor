2d987b4fbdd0044cbb86eae1a6bd84baaa8dd7c0
Eduardo <eduardoluissd@gmail.com>
2020-03-13 10:21:22 -0600

    Change types toget store efficiency

--- preprocessors/BDIPreprocessor.py
+++ preprocessors/BDIPreprocessor.py
@@ -6,21 +6,35 @@ from utils.CleanUtils import CleanUtils
 
 class BDIPreprocessor(Preprocessor):
 
+    def __init__(self):
+        # Connecting to DB
+        handler = DBHandler()
+        self.db = handler.get_client_db(SANTANDER_DB_NAME)
+        self.bdi = self.db['bdi']
+
     def parse(self):
-        # Pandas Manipulation
+        # DF Manipulation
         self.data = self.data.drop(columns=['Status', 'Referencia'])
         self.data = self.data.dropna(axis=1, how='all')
         self.data['Movimiento'] = self.data['Movimiento'].str.replace('#N/D', 'ACTIVO')
+        self.data['Adquisicion'] = self.data['Adquisicion'].apply(lambda x: CleanUtils.date_from_string(x))
+        self.data['Alta'] = self.data['Alta'].apply(lambda x: CleanUtils.date_from_string(x))
+        self.data['Fecha_Movimiento'] = self.data['Fecha_Movimiento'].apply(lambda x: CleanUtils.date_from_string(x))
+        self.data[['Fecha_Movimiento']] = self.data[['Fecha_Movimiento']].astype(object).where(self.data[['Fecha_Movimiento']].notnull(), None)
 
         # DF became a records
         self.data = self.data.to_dict('records')
         # Translate the keys in each record for standarize porpuse.
         self.data = [CleanUtils.translate_keys(record) for record in self.data]
+        # Connecting to DB
+        handler = DBHandler()
+        self.db = handler.get_client_db(SANTANDER_DB_NAME)
+        self.bdi = self.db['bdi']
 
     def upload(self):
-        handler = DBHandler()
-        db = handler.get_client_db(SANTANDER_DB_NAME)
-        bdi = db['bdi']
         print('Uploading...')
-        bdi.insert(self.data)
+        self.bdi.insert(self.data)
         print("Done!")
+
+    def remove(self):
+        self.bdi.remove()
