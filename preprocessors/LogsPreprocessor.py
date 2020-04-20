57f0a2e09469e3b4aa527f9baa82d8e441651b18
Eduardo <eduardoluissd@gmail.com>
2020-04-20 09:19:38 -0500

    Fixing bugs when one of the input list is empty

--- preprocessors/LogsPreprocessor.py
+++ preprocessors/LogsPreprocessor.py
@@ -23,15 +23,25 @@ class LogsPreprocessor(Preprocessor):
         # DF Manipulation
         if not self.daily_data.empty:
             self.daily_report_parser()
+        else:
+            self.daily_data = []
+
         if not self.weekly_data.empty:
             self.weekly_report_parser()
+        else:
+            self.weekly_data = []
 
         self.data = self.weekly_data or [] + self.daily_data or []
 
     # override
     def read(self, paths, **read_options):
+
+        print("--- Reading ---")
+
         weekly_file_paths = []
         daily_file_paths = []
+        weekly_data = pd.DataFrame()
+        daily_data = pd.DataFrame()
 
         # If input is a string put inside a list.
         if type(paths) == str:
@@ -54,7 +64,8 @@ class LogsPreprocessor(Preprocessor):
                 read_options['sheet_name'] = None
                 read_options['skiprows'] = 2
                 read_options['encoding'] = 'latin1'
-                weekly_data_temp = FileUtils.read(weekly_file_path,  **read_options)
+                weekly_data_temp = FileUtils.read(
+                    weekly_file_path,  **read_options)
 
                 if len(weekly_data_temp) > 1:
                     columns = weekly_data_temp['Ticket History IBM'].columns
@@ -63,7 +74,8 @@ class LogsPreprocessor(Preprocessor):
                         if sheet_name != "Ticket History IBM":
                             weekly_data_temp[sheet_name].columns = columns
 
-                    weekly_data_temp = pd.concat(weekly_data_temp, sort=False, ignore_index=True)
+                    weekly_data_temp = pd.concat(
+                        weekly_data_temp, sort=False, ignore_index=True)
                     weekly_data = pd.concat([weekly_data, weekly_data_temp],
                                             sort=False, ignore_index=True)
 
@@ -72,14 +84,17 @@ class LogsPreprocessor(Preprocessor):
 
     def upload(self):
         ids_on_db = self.update()
-        self.logs.insert([log for log in self.data if int(log['_id']) not in ids_on_db])
+        self.logs.insert(
+            [log for log in self.data if int(log['_id']) not in ids_on_db])
         print("Done!")
 
     def update(self):
+        print("--- Updating ---")
 
         # Update from daily data.
-        ids_on_db = [int(log['_id']) for log in self.logs_on_db]
-        logs_to_update = [log for log in self.data if log['_id'] in ids_on_db]
+        logs_on_db = {int(log['_id']): log for log in self.logs_on_db}
+        logs_to_update = [log for log in self.data if log['_id']
+                          in logs_on_db and log != logs_on_db[log['_id']]]
 
         for log in logs_to_update:
             _id = int(log['_id'])
@@ -89,7 +104,7 @@ class LogsPreprocessor(Preprocessor):
                     self.logs.replace_one({'_id': _id}, log_on_db, upsert=True)
                     print(f'Updating: {_id}')
 
-        return ids_on_db
+        return logs_on_db.keys()
 
     def remove(self):
         self.ticket.remove()
@@ -97,21 +112,27 @@ class LogsPreprocessor(Preprocessor):
     def daily_report_parser(self):
 
         # Drop unnecesary columns
-        columns_to_drop = ['REGIÓN', 'DURACIO', 'MES', 'AC', 'PROVEEDOR', 'MODELO', 'ATENCIÓN', 'SERVICIO']
+        columns_to_drop = ['REGIÓN', 'DURACIO', 'MES', 'AC',
+                           'PROVEEDOR', 'MODELO', 'ATENCIÓN', 'SERVICIO']
         self.daily_data = self.daily_data.drop(columns=columns_to_drop)
 
         # Handle Null end_date
         self.daily_data[['FECHA_FIN']] = self.daily_data[['FECHA_FIN']].astype(object).where(
-                                          self.daily_data[['FECHA_FIN']].notnull(), None)
+            self.daily_data[['FECHA_FIN']].notnull(), None)
 
         # Group the same logs and put the status in a list.
-        cols_group = [col for col in self.daily_data.columns if col != "ESTATUS"][14:]
+        cols_group = [
+            col for col in self.daily_data.columns if col != "ESTATUS"]
+        # cols_group = [col for col in self.daily_data.columns if col != "ESTATUS"][14:]
 
-        self.daily_data = self.daily_data.groupby(cols_group)['ESTATUS'].apply(list).reset_index()
+        self.daily_data = self.daily_data.groupby(
+            cols_group)['ESTATUS'].apply(list).reset_index()
 
         self.daily_data.rename(columns={'ID': 'atm'}, inplace=True)
-        self.daily_data.rename(columns={'TIPO FALLA': 'failure_type'}, inplace=True)
-        self.daily_data.rename(columns={'ESTATUS': 'daily_status'}, inplace=True)
+        self.daily_data.rename(
+            columns={'TIPO FALLA': 'failure_type'}, inplace=True)
+        self.daily_data.rename(
+            columns={'ESTATUS': 'daily_status'}, inplace=True)
         self.daily_data.rename(columns=CleanUtils.standarize_keys_dict(self.daily_data.columns),
                                inplace=True)
 
@@ -122,11 +143,13 @@ class LogsPreprocessor(Preprocessor):
 
         # Handle Null end_date
         self.weekly_data[['FECHA_FIN']] = self.weekly_data[['FECHA_FIN']].astype(object).where(
-                                           self.weekly_data[['FECHA_FIN']].notnull(), None)
+            self.weekly_data[['FECHA_FIN']].notnull(), None)
 
         # Group the same logs and put the status in a list.
-        cols_group = [col for col in self.weekly_data.columns if col != "STATUS"]
-        self.weekly_data = self.weekly_data.groupby(cols_group)['STATUS'].apply(list).reset_index()
+        cols_group = [
+            col for col in self.weekly_data.columns if col != "STATUS"]
+        self.weekly_data = self.weekly_data.groupby(
+            cols_group)['STATUS'].apply(list).reset_index()
 
         self.weekly_data.rename(columns={'ID': 'atm'}, inplace=True)
         self.weekly_data.rename(columns=CleanUtils.standarize_keys_dict(self.weekly_data.columns),
